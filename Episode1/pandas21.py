0a9e0830c1830a1c0c909cbf0ab449ab19ef972f
ALien <82714908+elfeliza@users.noreply.github.com>
2021-12-14 13:23:41 +0100

    Update pandas21.py

--- Episode1/pandas21.py
+++ Episode1/pandas21.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 data = pd.read_csv('transactions.csv', delimiter=',')
 print(data[data['STATUS']=='OK'].sort_values(by='SUM', ascending=False).head(3))
-data = pd.read_csv('transactions.csv', delimiter=',')
 data1 = data[data['CONTRACTOR']=='Umbrella, Inc']
 data2 = data1[data1['STATUS']=='OK']
 Total = data2['SUM'].sum()
